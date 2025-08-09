667ab204dcb4ea02b87dae0701f7d187fc73a0a6
Andreswr0 <andresrosd2@gmail.com>
2025-08-09 12:19:15 -0300

    actualiza el app.js

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = [];
+
+function agregarAmigo(){
+    let nombreInputId = document.getElementById("amigo");
+        if (nombreInputId.value.trim() !== "") {
+        amigos.push(nombreInputId.value);
+        mostrarAmigos();
+    }   else {
+            alert("Por favor, inserte un nombre.");
+    }
+}
+function mostrarAmigos() {
+    let listaAmigo = document.getElementById("listaAmigos");
+        listaAmigos.innerHTML = ""; 
+    for (let i = 0; i < amigos.length; i++){
+        let li = document.createElement("li");
+            li.textContent = amigos[i];
+            listaAmigo.appendChild(li);
+    }
+}
+
+function sortearAmigo(){
+    if (amigos.length < 2) {
+        alert('Necesitas dos amigos para realizar el sorteo')
+            return;
+    }
+    if (amigos.length > 10) {
+        alert( 'El maximo de amigos a sortear es 10')
+            return;
+    }
+        let indice = Math.floor(Math.random() * amigos.length);
+        let amigoSorteado = amigos[indice];
+        let resultado = document.getElementById("resultado");
+        let li = document.createElement("li");
+        li.textContent = "Tu amigo secreto es " + amigoSorteado;
+        resultado.appendChild(li);
+
+}
+// Con ayuda de los chicos de alura se pudo gracias <3
